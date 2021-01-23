1137337c8bf5ad42fb56123489dd5e45d809acdd
FruitRiin <nassii74@gmail.com>
2021-01-23 16:24:43 +0900

    routerファイルをいファクタ

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import axios from 'axios'
 
 import App from './App'
-import router from './router'
+import Router from 'vue-router'
+import routes from 'vue-auto-routing'
+
+Vue.use(Router)
+const router = new Router({
+  routes: routes
+})
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
