73cc4f3956d8d84a8120ef35be6abcbd2deb3d7d
FruitRiin <nassii74@gmail.com>
2021-02-01 22:32:36 +0900

    レイアウト修正、ショートカットキーを入力しやすく

--- src/renderer/pages/preference.vue
+++ src/renderer/pages/preference.vue
@@ -4,82 +4,82 @@
     <div class="container">
       <div class="col">
         認証済みアカウント
-        <div v-for="(token, key) in tokens" :key="key">
+        <div v-for="(token, key) in tokens" :key="key" class="row">
           [{{ key + 1 }}] {{ token.user }}
           <button @click="deleteAccount(key)">-</button>
-          <label>
-            <input v-model="token.shortcut.Shift" type="checkbox" />
-            Shift
-          </label>
-          <label>
-            <input v-model="token.shortcut.Ctrl" type="checkbox" />
-            Ctrl
-          </label>
-          <label>
-            <input v-model="token.shortcut.Alt" type="checkbox" />
-            Alt
-          </label>
-          +<input
+
+          <input v-model="token.shortcut.Shift" type="checkbox" />
+          <label>Shift</label>
+
+          <input v-model="token.shortcut.Ctrl" type="checkbox" />
+          <label>Ctrl</label>
+
+          <input v-model="token.shortcut.Alt" type="checkbox" />
+          <label>Alt</label>
+          +
+          <input
             v-model="token.shortcut.key"
+            class="shortcut_key"
             type="text"
             maxlength="1"
+            style="ime-mode: disabled"
+            @keypress="token.shortcut.key = ''"
             @input="token.shortcut.key = $event.target.value.toUpperCase()"
           />
         </div>
+        <button class="authenticate" @click="addAuth">アカウント認証</button>
       </div>
+      <div class="col">
+        <ul v-if="preference">
+          <li>
+            投稿ショートカット
+            <ul>
+              <li>
+                <label>
+                  <input
+                    v-model="preference.postShortcut"
+                    type="radio"
+                    value="shift"
+                  />
+                  Shift + Enter
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input
+                    v-model="preference.postShortcut"
+                    type="radio"
+                    value="ctrl"
+                  />
+                  Ctrl + Enter
+                </label>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <label>
+              <input
+                v-model="preference.alwaysOnTop"
+                type="checkbox"
+                :value="true"
+              />
+              投稿画面を常に最前面に表示
+            </label>
+          </li>
+          <li>
+            <label>
+              <input
+                v-model="preference.hideAfterPost"
+                type="checkbox"
+                :value="true"
+              />
+              投稿したらウィンドウを消す
+            </label>
+          </li>
+        </ul>
+      </div>
+    </div>
 
-      <button @click="addAuth">アカウント認証</button>
-    </div>
-    <div class="col">
-      <ul v-if="preference">
-        <li>
-          投稿ショートカット
-          <label>
-            <input
-              v-model="preference.postShortcut"
-              type="radio"
-              value="shift"
-            />
-            Shift + Enter
-          </label>
-          <label
-            ><input
-              v-model="preference.postShortcut"
-              type="radio"
-              value="ctrl"
-            />Ctrl + Enter</label
-          >
-        </li>
-        <li>
-          <label
-            ><input
-              v-model="preference.alwaysOnTop"
-              type="checkbox"
-              :value="true"
-            />投稿画面を常に最前面に表示</label
-          >
-        </li>
-        <li>
-          <label
-            ><input
-              v-model="preference.hideAfterPost"
-              type="checkbox"
-              :value="true"
-            />投稿したらウィンドウを消す</label
-          >
-        </li>
-        <li>
-          <label><input type="checkbox" disabled />投稿したら通知を表示</label>
-        </li>
-        <li>
-          <select id="" name="" disabled>
-            <option value="">位置を保存</option>
-            <option value="">思案中</option>
-</select
-          >表示位置
-        </li>
-      </ul>
-    </div>
     <div>
       <button @click="savePreference">保存</button>
     </div>
@@ -119,3 +119,26 @@ export default {
   },
 }
 </script>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+}
+
+.col {
+  width: 50%;
+}
+
+.row {
+  display: flex;
+  align-items: center;
+  width: 100%;
+
+  &:last-of-type {
+    margin-bottom: 2em;
+  }
+}
+.shortcut_key {
+  width: 1em;
+}
+</style>
