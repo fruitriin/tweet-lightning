55d276c2f52dad0b329271db658dbd9402023c3d
FruitRiin <nassii74@gmail.com>
2021-02-18 23:36:29 +0900

    vue-material-design-icons, bluma追加

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -11,5 +11,13 @@ export default {
 </script>
 
 <style>
-/* CSS */
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css";
+
+html {
+  overflow: auto;
+  min-width: auto;
+}
+.ui-icon {
+  line-height: 1;
+}
 </style>
