9e1e3ea40bc8693904d2ffc137328118dfc2dc74
FruitRiin <nassii74@gmail.com>
2021-02-19 00:08:53 +0900

    設定画面のデザインブラッシュアップ

--- src/renderer/pages/preference.vue
+++ src/renderer/pages/preference.vue
@@ -1,87 +1,119 @@
 <template>
-  <div>
-    <p>Pereference</p>
+  <div class="p-4">
+    <h2 class="title is-5">設定画面</h2>
     <div class="container">
       <div class="col">
-        認証済みアカウント
+        <p class="title is-6 mb-0">認証済みアカウント</p>
         <div v-for="(token, key) in tokens" :key="key" class="row">
-          [{{ key + 1 }}] {{ token.user }}
-          <button @click="deleteAccount(key)">-</button>
+          <div class="field is-flex">
+            <div style="width: 160px">[{{ key + 1 }}] {{ token.user }}</div>
+            <div>
+              <button class="delete" @click="deleteAccount(key)" />
+              <label class="checkbox">
+                <input
+                  :id="`shortcut-${key}-shift`"
+                  v-model="token.shortcut.Shift"
+                  class="checkbox"
+                  type="checkbox"
+                />
+                Shift
+              </label>
+              <label class="checkbox">
+                <input
+                  v-model="token.shortcut.Ctrl"
+                  class="checkbox"
+                  type="checkbox"
+                />
+                Ctrl
+              </label>
 
-          <input v-model="token.shortcut.Shift" type="checkbox" />
-          <label>Shift</label>
-
-          <input v-model="token.shortcut.Ctrl" type="checkbox" />
-          <label>Ctrl</label>
-
-          <input v-model="token.shortcut.Alt" type="checkbox" />
-          <label>Alt</label>
-          +
-          <input
-            v-model="token.shortcut.key"
-            class="shortcut_key"
-            type="text"
-            maxlength="1"
-            style="ime-mode: disabled"
-            @keypress="token.shortcut.key = ''"
-            @input="token.shortcut.key = $event.target.value.toUpperCase()"
-          />
+              <label class="checkbox">
+                <input
+                  v-model="token.shortcut.Alt"
+                  class="checkbox"
+                  type="checkbox"
+                />
+                Alt
+              </label>
+              +
+              <input
+                v-model="token.shortcut.key"
+                class="shortcut_key"
+                type="text"
+                maxlength="1"
+                style="ime-mode: disabled"
+                @keypress="token.shortcut.key = ''"
+                @input="token.shortcut.key = $event.target.value.toUpperCase()"
+              />
+            </div>
+          </div>
         </div>
         <button class="authenticate" @click="addAuth">アカウント認証</button>
       </div>
       <div class="col">
-        <ul v-if="preference">
-          <li>
-            投稿ショートカット
-            <ul>
-              <li>
-                <label>
-                  <input
-                    v-model="preference.postShortcut"
-                    type="radio"
-                    value="shift"
-                  />
-                  Shift + Enter
-                </label>
-              </li>
-              <li>
-                <label>
-                  <input
-                    v-model="preference.postShortcut"
-                    type="radio"
-                    value="ctrl"
-                  />
-                  Ctrl + Enter
-                </label>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <label>
-              <input
-                v-model="preference.alwaysOnTop"
-                type="checkbox"
-                :value="true"
-              />
-              投稿画面を常に最前面に表示
-            </label>
-          </li>
-          <li>
-            <label>
-              <input
-                v-model="preference.hideAfterPost"
-                type="checkbox"
-                :value="true"
-              />
-              投稿したらウィンドウを消す
-            </label>
-          </li>
-        </ul>
+        <template v-if="preference" />
+        <div class="mb-2">
+          <p class="title is-6 mb-0">投稿ショートカット</p>
+          <ul>
+            <li>
+              <label>
+                <input
+                  v-model="preference.postShortcut"
+                  type="radio"
+                  value="shift"
+                />
+                Shift + Enter
+              </label>
+            </li>
+            <li>
+              <label>
+                <input
+                  v-model="preference.postShortcut"
+                  type="radio"
+                  value="ctrl"
+                />
+                Ctrl + Enter
+              </label>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p class="title is-6 mb-0">詳細設定</p>
+          <ul>
+            <li>
+              <label>
+                <input
+                  v-model="preference.alwaysOnTop"
+                  type="checkbox"
+                  :value="true"
+                />
+                投稿画面を常に最前面に表示
+              </label>
+            </li>
+            <li>
+              <label>
+                <input
+                  v-model="preference.hideAfterPost"
+                  type="checkbox"
+                  :value="true"
+                />
+                投稿したらウィンドウを消す
+              </label>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
 
-    <div>
+    <div class="mb-6">
       <button @click="savePreference">保存</button>
+    </div>
+    <div>
+      <p class="title is-5 mb-0">ショートカットキー一覧</p>
+      <ul>
+        <li>[投稿フォーム]ESC ウィンドウを閉じる</li>
+        <li>[投稿フォーム](Shift/Ctrl)+Enter 投稿</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -135,10 +167,10 @@ export default {
   width: 100%;
 
   &:last-of-type {
-    margin-bottom: 2em;
+    margin-bottom: 0.5rem;
   }
 }
 .shortcut_key {
-  width: 1em;
+  width: 1.5rem;
 }
 </style>
