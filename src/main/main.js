edffd7eb2d81875bea8a26bbb73f5e2f700d2c60
fruitriin <nassii74@gmail.com>
2021-03-03 23:49:13 +0900

    ロギングの採番を修正

--- src/main/main.js
+++ src/main/main.js
@@ -3,20 +3,20 @@
 import { app, Menu } from "electron"
 import { createPostWindow, postWindow, openPreference } from "./modules/windows"
 import { store } from "./modules/store"
+import { machineIdSync } from "node-machine-id"
 require("./modules/events")
 require("./modules/tray")
 require("./modules/globalShortcuts")
 require("dotenv").config()
 
 const Nucleus = require("nucleus-nodejs")
-Nucleus.init("60258c87bcf16266e1db001d", { disableInDev: true })
-
-Nucleus.setProps({
-  version: app.getVersion(),
-})
-Nucleus.appStarted()
+Nucleus.init("60258c87bcf16266e1db001d", { disableInDev: true, debug: true })
+Nucleus.setUserId(machineIdSync())
 
 app.on("ready", () => {
+  Nucleus.setProps({ version: process.env.npm_package_version })
+  Nucleus.appStarted()
+
   const accounts = store.get("accounts") || []
   if (accounts.length > 0) {
     createPostWindow()
